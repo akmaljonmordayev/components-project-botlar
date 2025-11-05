ccdfa7376836543cdb34f5a3320583d6367c17be
MacBro <m1@iMac-MacBro.local>
2025-11-05 20:46:38 +0500

    Zokhidbek done

--- src/compoennts/Main/Section1/Section1.jsx
+++ src/compoennts/Main/Section1/Section1.jsx
@@ -2,8 +2,18 @@ import React from "react";
 import "./Section1.css";
 function Section1() {
   return (
-    <section>
-      <div className="container">Section1</div>
+    <section className="firstSection">
+      <div className="container">
+        <h3>Food app</h3>
+        <h2>Why stay hungry when 
+        you can order form Bella Onojie</h2>
+        <p>Download the bella onoje’s food app now on</p>
+        <div className="buttons">
+          <button className="firstButton">Playstore</button>
+          <button className="secondButton">App store</button>
+        </div>
+      </div>
+      <img src="/public/images/Group 53.png" alt="" />
     </section>
   );
 }
