6d42cb58c7c07a5a1b81deccf417f06c3345fb68
tohirbek6768-png <tohirbek6768@gmail.com>
2025-11-06 14:12:53 +0500

    tohir done

--- src/compoennts/Header/Header.jsx
+++ src/compoennts/Header/Header.jsx
@@ -1,10 +1,25 @@
 import React from "react";
 import "./Header.css";
+import logo from "/public/images/logo.png";
 function Header() {
   return (
     <header>
       <div className="container">
-        <nav>Header</nav>
+        <nav>
+          <div className="">
+            <img src={logo} alt="" />
+          </div>
+          
+
+          <div className="header1">
+            <ul>
+              <li><a href="">Home</a></li>
+              <li><a href="">Product</a></li>
+              <li><a href="">Faq</a></li>
+              <li><a href="">Contact</a></li>
+            </ul>
+          </div>
+        </nav>
       </div>
     </header>
   );
