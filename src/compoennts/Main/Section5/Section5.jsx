fbbddd4d17764a454fbf9c307f33f94bd3da697a
boxo0009 <b37928799@gmail.com>
2025-11-06 18:54:11 +0500

    footer vs section done

--- src/compoennts/Main/Section5/Section5.jsx
+++ src/compoennts/Main/Section5/Section5.jsx
@@ -2,8 +2,17 @@ import React from "react";
 import "./Section5.css";
 function Section5() {
   return (
-    <section>
-      <div className="container">Section5</div>
+    <section className="section5">
+      <div className="container">
+        <div className="teext2">
+          <h1 className="text" >Download the app now.</h1>
+          <p className="ptext" >Available on your favorite store. Start your premium experience now</p>
+        </div>
+        <div className="section-btn">
+          <button className="btnbtn" >Playstore</button>
+          <button className="btnbtn" >App store</button>
+        </div>
+      </div>
     </section>
   );
 }
