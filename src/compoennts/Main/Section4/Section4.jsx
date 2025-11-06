4e80498b069cfa3d21d3b5ae02c640e0380dceda
hisamutdinovakbar48-lgtm <hisamutdinovakbar48@gmail.com>
2025-11-06 14:21:01 +0500

    asdasdvasdvdas

--- src/compoennts/Main/Section4/Section4.jsx
+++ src/compoennts/Main/Section4/Section4.jsx
@@ -1,22 +1,20 @@
 import React from "react";
 import "./Section4.css";
-import img from "/public/Rectangle (1).png"
+import img from "/public/Rectangle (1).png";
 function Section4() {
   return (
     <section>
       <div className="container">
-<div className="doda">
-  <div className="otas">
-    <img src={img} alt="" />
-  </div>
-  <div className="bolas">
-    <p>Checkout</p>
-    <h3>When you done check out
-and get it delivered.</h3>
-<h5>When you done check out and get it 
-delivered with ease.</h5>
-  </div>
-</div>
+        <div className="doda">
+          <div className="otas">
+            <img src={img} alt="" />
+          </div>
+          <div className="bolas">
+            <p>Checkout</p>
+            <h3>When you done check out and get it delivered.</h3>
+            <h5>When you done check out and get it delivered with ease.</h5>
+          </div>
+        </div>
       </div>
     </section>
   );
