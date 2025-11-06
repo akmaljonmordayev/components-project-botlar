b61909bb6e9585272e6d640e4ffdc208ec87eed6
hisamutdinovakbar48-lgtm <hisamutdinovakbar48@gmail.com>
2025-11-06 12:13:08 +0500

    akbar done

--- src/compoennts/Main/Section3/Section3.jsx
+++ src/compoennts/Main/Section3/Section3.jsx
@@ -1,9 +1,21 @@
 import React from "react";
 import "./Section3.css";
+import logo1 from "/public/logo1.png";
 function Section3() {
   return (
-    <section>
-      <div className="container">Section3</div>
+    <section className="sect3">
+      <div className="container">
+        <div className="parent">
+          <div className="left-div">
+            <p className="pp">Explore varieties</p>
+            <h1>Shop for your favorites meal as e dey hot.</h1>
+            <p>Shop for your favorite meals or drinks and enjoy while doing it.</p>
+          </div>
+          <div className="right-div">
+            <img src={logo1} alt="" />
+          </div>
+        </div>
+      </div>
     </section>
   );
 }
